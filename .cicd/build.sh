33afce6fed2d2bea2d52a1e4f8f97ff760133e23
Joseph Moore <yo.yo.yossarian@gmail.com>
2019-01-24 22:26:24 -0700

    Add pipeline

---
+++ .cicd/build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "Building Kotlin Model"
+cd kotlin && ./mvnw clean install
---
+++ .cicd/deploy.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "Deploying Kotlin Model"
+cd kotlin && ./mvnw deploy
