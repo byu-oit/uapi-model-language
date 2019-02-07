21971ab6ace350bb6c2548e8c53e929ca3d3aabf
Joseph Moore <yo.yo.yossarian@gmail.com>
2019-02-07 01:31:19 -0700

    add extension and mime info to ser format

--- kotlin/model/src/main/kotlin/serialization/UAPISerializationFormat.kt
+++ kotlin/model/src/main/kotlin/serialization/UAPISerializationFormat.kt
@@ -1,5 +1,9 @@
 package edu.byu.uapi.model.serialization
 
-enum class UAPISerializationFormat {
-    JSON, YAML
+enum class UAPISerializationFormat(
+    val extension: String,
+    val mime: String
+) {
+    JSON("json", "application/json"),
+    YAML("yml", "text/x-yaml")
 }
